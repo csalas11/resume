9afff70f0158f958643b56d48d8e8509031de4ff
csalas11 <salascarlos11@gmail.com>
2017-09-25 00:36:57 -0700

    Added working theme, also added personalized images and buttons in order to make it more personalized.

---
+++ app/assets/javascripts/freelancer/manifest.js
@@ -0,0 +1,6 @@
+//= require bootstrap
+//= require freelancer/classie
+//= require freelancer/cbpAnimatedHeader
+//= require freelancer/contact_me
+//= require freelancer/freelancer
+//= require freelancer/jqBootstrapValidation
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'salascarlos11@gmail.com'
+  layout 'mailer'
+end
